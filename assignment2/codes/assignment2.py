727bdf0379f81ff69b19bd46508891ab18de65b7
ayush-2321 <80596567+ayush-2321@users.noreply.github.com>
2021-10-29 01:06:22 +0530

    Add files via upload

---
+++ assignment2/codes/assignment2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 27 01:06:11 2021
+@author: singh
+"""
+
+import numpy as np
+A=np.array([[1,2,3],[2,3,1]])
+B=np.array([[3,-1,3],[-1,0,2]])
+C= 2*A-B
+print(C)
+# hence, verified
